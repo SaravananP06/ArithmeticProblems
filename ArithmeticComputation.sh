e2c1b26ec313579ef196b3086e095120c5d73017
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-06-03 18:14:07 +0530

    [Saravanan]: Added 3 number input program in ArithmeticProblem

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -1,1 +1,11 @@
+#!/bin/bash -x
 echo Welcome to arithmetic computaion
+
+#UC1
+read -p "Enter first number: " a
+read -p "Enter second number: " b
+read -p "Enter third number: " c
+
+echo "a = " $a
+echo "b = " $b
+echo "c = " $c
