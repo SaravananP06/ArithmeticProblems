3bc06b8f8b5815daa9714dfe8b808c63ac74c990
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-06-03 18:39:16 +0530

    [Saravanan]: Computing the 3 inputs var in ArithmeicProblems

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -2,6 +2,7 @@
 echo Welcome to arithmetic computaion
 
 #UC1
+
 read -p "Enter first number: " a
 read -p "Enter second number: " b
 read -p "Enter third number: " c
@@ -9,3 +10,10 @@ read -p "Enter third number: " c
 echo "a = " $a
 echo "b = " $b
 echo "c = " $c
+
+#UC2
+
+res1=$(($a+($b*$c)))
+
+echo "a+b*c = " $res1
+
